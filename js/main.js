86f057fd897efb97d9ddf0206bccac44c85d07e1
Youssef-AJDIJ <ajdijyoussef@gmail.com>
2025-08-09 20:16:01 +0200

    añadir event target al menu

--- js/main.js
+++ js/main.js
@@ -1,14 +1,29 @@
 const toggleList = document.getElementById("toggle-list");
 const openMenu = document.getElementById("open-menu");
 const closeMenu = document.getElementById("close-menu");
+const toggleMenu = document.querySelector(".toggle-menu");
 
-openMenu.addEventListener("click",()=> {
-    openMenu.setAttribute("data-toggle","close")
-    closeMenu.setAttribute("data-toggle","open")
-    toggleList.setAttribute("data-list", "list-show")
-})
-closeMenu.addEventListener("click",()=> {
-    closeMenu.setAttribute("data-toggle","close")
-    openMenu.setAttribute("data-toggle","open")
-    toggleList.setAttribute("data-list", "list-hidden")
-})
+document.addEventListener("click", (event) => {
+
+  if (
+    !toggleList.contains(event.target) &&
+    !openMenu.contains(event.target) &&
+    !closeMenu.contains(event.target) &&
+    !toggleMenu.contains(event.target)
+  ) {
+    closeMenu.setAttribute("data-toggle", "close");
+    openMenu.setAttribute("data-toggle", "open");
+    toggleList.setAttribute("data-list", "list-hidden");
+  }
+});
+
+openMenu.addEventListener("click", () => {
+  openMenu.setAttribute("data-toggle", "close");
+  closeMenu.setAttribute("data-toggle", "open");
+  toggleList.setAttribute("data-list", "list-show");
+});
+closeMenu.addEventListener("click", () => {
+  closeMenu.setAttribute("data-toggle", "close");
+  openMenu.setAttribute("data-toggle", "open");
+  toggleList.setAttribute("data-list", "list-hidden");
+});
